6c9ffdf0364e60aa1388fd968a19038e922d98aa
Naveed Massjouni <naveedm9@gmail.com>
2013-05-20 03:23:37 -0400

    updated gateway interface

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
+# NAME
+
+AnyMerchant - A generic and consistent interface for online payment services
+
+# VERSION
+
+version 0.0002
+
 # SYNOPSIS
 
     my $gateway = AnyMerchant->gateway('Balanced', password => 'abc123');
@@ -20,3 +28,14 @@ should implement.
 This is a static factory method that creates an AnyMerchant::Gateway::\* object,
 validates its interface, and returns it.
 In the above example, an AnyMerchant::Gateway::Balanced object is returned.
+
+# AUTHOR
+
+Naveed Massjouni <naveedm9@gmail.com>
+
+# COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2013 by Naveed Massjouni.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
--- lib/AnyMerchant/Gateway.pm
+++ lib/AnyMerchant/Gateway.pm
@@ -1,7 +1,7 @@
 package AnyMerchant::Gateway;
 use Moo::Role;
 
-requires qw(authorize capture charge refund void);
+requires qw(debit authorize capture credit void);
 
 =head1 DESCRIPTION
 
@@ -9,23 +9,39 @@ This role defines the interface for all C<AnyMerchant::Gateway::* classes>.
 
 =head1 METHODS
 
+The param C<$amount> is an integer representing cents.
+An amount of 1000 corresponds to $10.00.
+
+The param C<$source> refers to either a credit card or a bank account.
+It may be a proper object or a hashref.
+
+=head2 debit
+
+    debit($amount, $source, %params)
+
+Charge a credit card or debit a bank account.
+
 =head2 authorize
+
+    authorize($amount, $source, %params)
 
 Authorize a transaction.
 
 =head2 capture
 
+    capture($amount, $id, %params)
+
 Capture an authorized transaction.
 
-=head2 debit
+=head2 credit
 
-Debit a credit card or bank account.
+    credit($amount, $target, %params)
 
-=head2 refund
-
-Refund a transaction.
+Credit account.
 
 =head2 void
+
+    void($id, %params)
 
 Void a transaction.
 
